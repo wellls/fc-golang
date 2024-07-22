8f028ecc0bd63e8b8f888c0069e5f239cb0cc7e7
Wellington Lima <wellington.map@hotmail.com>
2024-07-22 09:50:23 -0300

    declaration and attr

---
+++ 2/main.go
@@ -0,0 +1,15 @@
+package main
+
+const a = "Hello, World!"
+
+var (
+	b bool    = true
+	c int     = 10
+	d string  = "Teste"
+	e float64 = 1.2
+)
+
+func main() {
+	a := "X" // string
+	println(a)
+}
